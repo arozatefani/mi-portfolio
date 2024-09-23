b73e473b6979547c8b67dd96d578420d0159fb47
Tefani <119545375+arozatefani@users.noreply.github.com>
2024-09-22 21:58:39 -0300

    primera subida
    portfolio inicial

---
+++ codigo.js
@@ -0,0 +1,7 @@
+
+    function saludar(){
+        var user =document.getElementById("nombre").value;
+        var mail = document.getElementById("email").value;
+        alert ("tu mensaje fue enviado  " + user);
+        console.log("hola" + user);
+    }
